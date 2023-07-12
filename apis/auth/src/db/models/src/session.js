7557becbe3f4029b70746c58f9db529daf8ea6cd
Craig <c.chick@tx-consulting.com>
2023-07-12 17:00:11 +0200

    Version 1

--- apis/auth/src/db/models/src/session.js
+++ apis/auth/src/db/models/src/session.js
@@ -1,14 +1,9 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema({
-  token: String,
-  createdAt: { type: Date, default: new Date() },
-});
-
-if (Boolean(process.env.EXPIRE_SESSION_TOKEN)) {
-  let seconds = Number(process.env.SESSION_TOKEN_EXPIRATION_TIME);
-  if (isNaN(seconds)) seconds = false;
-  Schema.index({ createdAt: 1 }, { expireAfterSeconds: seconds || 604800 });
-}
-
-export default mongoose.model('session', Schema);
+export default mongoose.model(
+  'session',
+  mongoose.Schema({
+    token: String,
+    createdAt: { type: Date, default: new Date() },
+  })
+);
